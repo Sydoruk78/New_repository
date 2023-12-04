8971a97f2e4b1a7f62eb041c05aa2a6a20271c19
Sydoruk78 <sidorukkonstantin9@gmail.com>
2023-12-04 13:27:29 +0100

    Deleted time modul

--- modules/ui/page_objects/start_page_postua.py
+++ modules/ui/page_objects/start_page_postua.py
@@ -3,7 +3,7 @@ from modules.ui.page_objects.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 class StartPagePoctUA(BasePage):
     URL = 'https://novaposhta.ua'
